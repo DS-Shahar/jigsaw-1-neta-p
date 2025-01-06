725b529d8cecf97a3487a47bdc9ab212fd6d180a
neta-p <netapernat@gmail.com>
2025-01-06 14:46:22 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,71 +1,59 @@
-package lesson1;
-	import java.util.*;
-	public class listlevel2 {
-		public static Scanner input = new Scanner(System.in);
-		public static Node<Integer> BuildNode(Node<Integer> a) 
-		{
-	        Node<Integer> current = a;
-	        while (current != null && current.getNext() != null) 
-	        {
-	            Node<Integer> runner = current;
-	            while (runner.getNext() != null) 
-	            {
-	                if (runner.getNext().getValue() == current.getValue()) 
-	                {
-	                    runner.setNext(runner.getNext().getNext());
-	                } 
-	                else 
-	                {
-	                    runner = runner.getNext();
-	                }
-	            }
-	            current = current.getNext();
-	        }
+public class Main {
 
-	        return a;
+	public static Node<Integer> delEven(Node<Integer> H) {
+		Node<Integer> current = H;
+		Node<Integer> newH = new Node<>(null);
+		Node<Integer> newCurrent = newH;
+		while (current.getNext().getNext()!=null) {
+			newCurrent.setNext(current.getNext());
+			newCurrent = current.getNext();
+			current.setNext(current.getNext().getNext());
+			current = current.getNext();
+		}
+		newCurrent.setNext(current.getNext());
+		current.setNext(current.getNext().getNext());
+		return newH.getNext();
+	}
+
+	public static Node<Integer> delDoubles(Node<Integer> head) {
+	    Node<Integer> current1 = head;
+
+	    while (current1 != null) {
+	        Node<Integer> current2 = current1.getNext();
+	        Node<Integer> previous2 = current1;
+
+	        while (current2 != null) {
+	            if (current2.getValue()==current1.getValue()) {
+	                previous2.setNext(current2.getNext()); 
+	            } else {
+	                previous2 = current2;  
+	            }
+	            current2 = current2.getNext();  
+	        }
+	        current1 = current1.getNext();   
 	    }
-		public static Node<Integer> eraseEven(Node<Integer> a)
-		{
-		    if (a == null) 
-		    {
-		        return null;
-		    }
-		    Node<Integer> current = a;
-		    Node<Integer> previous = null;
-		    int index = 1;
-		    while (current != null) 
-		    {
-		        if (index % 2 == 0) 
-		        {
-		            if (previous != null) 
-		            {
-		                previous.setNext(current.getNext());
-		            } 
-		            else 
-		            {
-		                a = current.getNext();
-		            }
-		        }
-		        else 
-		        {
-		            previous = current;
-		        }
-		        current = current.getNext();
-		        index++;
-		    }
-		    return a;
-		}
-		public static void main(String[] args) {	
-			Node<Integer> a = new Node<Integer>(7);
-			Node<Integer> b = new Node<Integer>(2);
-			Node<Integer> c = new Node<Integer>(7);
-			Node<Integer> d = new Node<Integer>(5);
-			Node<Integer> e = new Node<Integer>(2);
-			a.setNext(b);
-			b.setNext(c);
-			c.setNext(d);
-			d.setNext(e);
-			System.out.println(BuildNode(a));
-			System.out.println(eraseEven(a));
-		}
-}
+
+		return head;
+	}
+
+	public static void main(String[] args) {
+		Node<Integer> a1 = new Node<>(1);
+		Node<Integer> a2 = new Node<>(4);
+		Node<Integer> a3 = new Node<>(1);
+		Node<Integer> a4 = new Node<>(4);
+		Node<Integer> a5 = new Node<>(5);
+		Node<Integer> a6 = new Node<>(6);
+		Node<Integer> a7 = new Node<>(7);
+		Node<Integer> a8 = new Node<>(7);
+
+		a1.setNext(a2);
+		a2.setNext(a3);
+		a3.setNext(a4);
+		a4.setNext(a5);
+		a5.setNext(a6);
+		a6.setNext(a7);
+		a7.setNext(a8);
+
+		System.out.println(delDoubles(a1));
+
+	}
